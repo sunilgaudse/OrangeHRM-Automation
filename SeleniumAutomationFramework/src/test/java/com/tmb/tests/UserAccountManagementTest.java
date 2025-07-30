b0173ca4455d98024abcff1a7f45b1e37033855d
sunilgaudse <125111272+sunilgaudse@users.noreply.github.com>
2025-07-30 16:57:19 +0530

    Hrm 1.0 (#9)
    * added login test cases
    
    * added invali cred test case
    
    * Added verifyThatAdminCannotLogInWithOnlyUsernameFilled test
    
    * Added password only test case
    
    * Added new Page OrangeUpdatePasswordPage, Test OrangeHRMUpdateTest,Change
    in dta.xlxs
    
    * resolved conflicts
    
    * Added browser to all tests.
    
    * Added Custom annotaion @Sheet to framwork.
    
    * Added new test case and also added logic to read  multiple sheets from
    excel in excelutils
    
    * Added verifyThatUserCannotChangePasswordWhenTheNewPasswordsDoNotMatch
    test case under UserAccountManagement.
    
    * Added output
    
    * Added output 2
    
    * removed conflicts
    
    * Added op 3
    
    * op 4
    
    * Added verifyThatUserCannotChangePasswordWhenThePasswoedDoesNotMeetSpecifiedCriteria
    test case

--- SeleniumAutomationFramework/src/test/java/com/tmb/tests/UserAccountManagementTest.java
+++ SeleniumAutomationFramework/src/test/java/com/tmb/tests/UserAccountManagementTest.java
@@ -60,5 +60,18 @@ public class UserAccountManagementTest extends BaseTests{
 		Assertions.assertThat(passwordMissmatchString)
 		.contains("Passwords do not match");
 	}
+	@Test
+	public void verifyThatUserCannotChangePasswordWhenThePasswoedDoesNotMeetSpecifiedCriteria(Map<String, String>data) {
+		try {
+			ExtentLogger.pass("Test is running in "+ data.get("browser") +" browser." , true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String criteriaText = new OrangeHRMLoginPage().enterUsername(data.get("username")).enterPassword(data.get("password")).clickLogin()
+				.clickWelcome().clickChangePassword().enterCurrentPassword(data.get("currentpassword")).enterNewPassword(data.get("newpassword"))
+				.getCriteriaText();
+		Assertions.assertThat(criteriaText)
+		.contains("Should have at least 7 characters");
+	}
 
 }
