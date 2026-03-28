d8713d3d2b9fec474c44fb55c4b83d5c6ea7175f
sunilgaudse <125111272+sunilgaudse@users.noreply.github.com>
2026-03-28 14:35:56 +0530

    Added logging functionality (#19)
    * Fixed गलत merge conflict markers in RetryFailedTests
    
    * Added logger functionality to test and pages and listner class using
    log4j2

--- SeleniumAutomationFramework/src/main/java/com/tmb/pages/BasePage.java
+++ SeleniumAutomationFramework/src/main/java/com/tmb/pages/BasePage.java
@@ -1,15 +1,19 @@
 package com.tmb.pages;
 
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.tmb.driver.DriverManager;
 import com.tmb.reports.ExtentLogger;
 
 public class BasePage {
+	public static Logger logger = LoggerFactory.getLogger(BasePage.class);
 	protected void click(By by,String elementName) {
 		DriverManager.getDriver().findElement(by).click();
 		try {
 			ExtentLogger.pass(elementName + " clicked.", true);
+			logger.info(elementName + " clicked.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -18,6 +22,7 @@ public class BasePage {
 		DriverManager.getDriver().findElement(by).sendKeys(value);
 		try {
 			ExtentLogger.pass(value +" is entered successfully in "+ elementName, true);
+			logger.info(value +" is entered successfully in "+ elementName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
